d8621b2cd18a7305d70cead6adea2fa14be6ff6b
Anna Kravets <annakravets1906@gmail.com>
2024-03-09 10:13:42 +0200

    Added Backend and Frontend

---
+++ backend/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+// import medicineRouter from './routes/api/medicine-router.js';
+
+const app = express();
+
+// app.use('/api/', medicineRouter);
+
+export default app;
